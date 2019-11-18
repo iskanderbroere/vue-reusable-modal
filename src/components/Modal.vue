3e828e4f207bd142915cf73a54bf334c21cc48e1
Iskander Broere <iskanderbroere@gmail.com>
2019-11-18 14:19:00 +0100

    refactor: to component library

---
+++ src/components/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-show="isOpen">Hello there!</div>
+</template>
+
+<script lang="ts">
+import { createComponent, ref } from "@vue/composition-api";
+
+export default createComponent({
+  setup() {
+    const isOpen = ref(true);
+    return { isOpen };
+  }
+});
+</script>
