5a32fffcecdc20ca60ebe89d5c70c0d5f10bd8de
Iskander Broere <iskanderbroere@gmail.com>
2019-11-26 13:44:58 +0100

    fix: disable eslint rule for next line

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -24,14 +24,14 @@ function checkAriaLabels(props: any): void {
   const details =
     "\nSee https://www.w3.org/TR/wai-aria/#ariaLabel for details.";
   if (!props["ariaLabel"] && !props["ariaLabelledBy"]) {
-    // eslint-disable-line no-console
+    // eslint-disable-next-line no-console
     console.error(
       `A modal/dialog must have either an \`ariaLabel\` or \`ariaLabelledBy\` prop.
       ${details}`
     );
   }
   if (props["ariaLabel"] && props["ariaLabelledBy"]) {
-    // eslint-disable-line no-console
+    // eslint-disable-next-line no-console
     console.error(
       "You provided both `ariaLabel` and `ariaLabelledBy` props to a modal/dialog. If the a label for this component is visible on the screen, that label's component should be given a unique ID prop, and that ID should be passed as the `ariaLabelledBy` prop into modal/dialog. If the label cannot be determined programmatically from the content of the element, an alternative label should be provided as the `ariaLabel` prop, which will be used as an `ariaLabel` on the HTML tag." +
         details
