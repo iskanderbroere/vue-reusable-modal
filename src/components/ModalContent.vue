0135e1d6a7d3ae436f1d024336859b65210f01fc
Iskander Broere <iskanderbroere@gmail.com>
2019-12-03 07:39:18 +0100

    feat: simple css build

--- src/components/ModalContent.vue
+++ src/components/ModalContent.vue
@@ -9,13 +9,3 @@
     <slot />
   </div>
 </template>
-
-<style>
-[data-modal-content] {
-  width: 50vw;
-  margin: 10vh auto;
-  background: white;
-  padding: 2rem;
-  outline: none;
-}
-</style>
--- src/components/ModalInner.vue
+++ src/components/ModalInner.vue
@@ -59,14 +59,3 @@ export default {
 };
 </script>
 
-<style>
-[data-modal-overlay] {
-  background: hsla(0, 0%, 0%, 0.33);
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  overflow: auto;
-}
-</style>
